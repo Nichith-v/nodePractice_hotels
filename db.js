ec04a491d21ef9c4ddad27566c791332413faf04
Nichith V <vnichith2003@gmail.com>
2024-06-02 07:08:38 +0530

    not showing security information

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Define MongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/hotels';
+const mongoURL = process.env.MONGODB_URL_LOCAL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
